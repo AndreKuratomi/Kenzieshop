e0c8896814b19bef415354052c464a1a1826c298
André Bergamaschi Kuratomi <68620412+AndreKuratomi@users.noreply.github.com>
2022-04-04 15:45:44 -0300

    Update products.js

--- src/products/products.js
+++ src/products/products.js
@@ -27,33 +27,3 @@ const products = [
 ];
 
 export default products;
-
-// const products = [
-//   {
-//     name: "Patinho altivo",
-//     price: 99.99,
-//     img: "https://i.imgur.com/UztqwRNb.jpg",
-//   },
-//   {
-//     name: "Patinho esponja",
-//     price: 88.4,
-//     img: "https://i.imgur.com/FyKYxP2b.jpg",
-//   },
-//   {
-//     name: "Patinho Clérigo",
-//     price: 78.6,
-//     img: "https://i.imgur.com/mawgEQ5b.jpg",
-//   },
-//   {
-//     name: "Patinho master",
-//     price: 86000,
-//     img: "https://i.imgur.com/kwuJ5nlb.jpg",
-//   },
-//   {
-//     name: "Patinhos islâmicos",
-//     price: 268,
-//     img: "https://i.imgur.com/kGtqu7Eb.jpg",
-//   },
-// ];
-
-// export default products;
